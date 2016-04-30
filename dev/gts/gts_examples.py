b3b21ca27a031f78ae5d2ef1814fab020b1a04c1
juzhang <jzha263@aucklanduni.ac.nz>
2016-05-01 09:00:01 +1200

    updated gts dev script

--- dev/gts/gts_examples.py
+++ dev/gts/gts_examples.py
@@ -1,10 +1,12 @@
 """
 Examples and tests scratch pad for using the GNU Triangulated Surface Library
 """
-
-from gias2.mesh import vtktools
+import numpy as np
+from gias2.mesh import vtktools, simplemesh, gtstools
+from gias2.visualisation import fieldvi
 import gts
 
+#=============================================================================#
 # example surface
 surf_file = '2008_1741_tibia_fibula.wrl'
 reader = vtktools.Reader()
@@ -12,20 +14,43 @@ reader.read(surf_file)
 smesh = reader.getSimplemesh()
 
 # converting between a simplemesh and a GTS surface
+gts_surface = gtstools.simplemesh2gtssurf(smesh)
+smesh_gts = gtstools.gtssurf2simplemesh(gts_surface)
 
-## create vertices
-gts_verts = [gts.Vertex(*vi) for vi in smesh.v]
+# generating simple shapes
+# gts_cube = gtstools.gts.cube()
+gts_sphere = gtstools.gts.sphere(4)
 
-## create surface
-gts_surf = gts.Surface()
-for fi in smesh.f:
-    ## create edges, anticlockwise by indices in f
-    e1 = gts.Edge(gts_verts[fi[0]], gts_verts[fi[1]])
-    e2 = gts.Edge(gts_verts[fi[1]], gts_verts[fi[2]])
-    e3 = gts.Edge(gts_verts[fi[2]], gts_verts[fi[0]])
+# transform shapes
+target = smesh.v.mean(0)
+gts_sphere.scale(50,50,50)
+gts_sphere.translate(*target)
+smesh_sphere = gtstools.gtssurf2simplemesh(gts_sphere)
 
-    ## create face
-    gts_surf.add(gts.Face(e1, e2, e3))
+# cylinder/trunc cone
+gts_cylinder = gtstools.cylinder(
+    start=smesh.v.min(0),
+    end=smesh.v.max(0),
+    startr=10.0,
+    endr=10.0,
+    slices=32,
+    stacks=50,
+    )
+smesh_cylinder = gtstools.gtssurf2simplemesh(gts_cylinder)
+vtktools.Writer(v=smesh_cylinder.v, f=smesh_cylinder.f).write('cylinder.ply')
 
-# read out vertices and faces
-coords = np.array([v.coords() for f in gts_surf.vertices()])
+# boolean
+gts_surface_diff = gts_surface.difference(gts_cylinder)
+gts_surface_union = gts_surface.union(gts_cylinder)
+smesh_diff = gtstools.gtssurf2simplemesh(gts_surface_diff)
+smesh_union = gtstools.gtssurf2simplemesh(gts_surface_union)
+
+# view
+v = fieldvi.Fieldvi()
+v.addTri('original', smesh)
+v.addTri('gts', smesh_gts)
+v.addTri('gts sphere', smesh_sphere)
+v.addTri('gts cylinder', smesh_cylinder)
+v.addTri('diff', smesh_diff)
+v.addTri('union', smesh_union)
+v.configure_traits()
