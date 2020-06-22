fix matrix multiplication in SVD calculation of rigid transform matrix

tests/micro/test_transform3D.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 from generator import generator, generate
 
-from gias2.common.transform3D import directAffine
+from gias2.common.transform3D import directAffine, calcAffineMatrixSVD, transformAffine
 
 point_set_0 = np.array([
     [0, 0, 0],
@@ -50,4 +50,16 @@ class TestTransform3D(unittest.TestCase):
     )
     def test_direct_affine(self, name: str, source: np.ndarray, target: np.ndarray, expected_mat: np.ndarray):
         matrix: np.ndarray = directAffine(source, target)
+        transformed = transformAffine(source, matrix)
+        self.assertTrue(np.allclose(transformed, target, equal_nan=True))
+        self.assertTrue(np.allclose(matrix, expected_mat, equal_nan=True))
+
+    @generate(
+        ('identity', point_set_0, point_set_0, t_matrix_0),
+        ('translation', point_set_0, point_set_1, t_matrix_1),
+    )
+    def test_calc_affine_matrix_svd(self, name: str, source: np.ndarray, target: np.ndarray, expected_mat: np.ndarray):
+        matrix: np.ndarray = calcAffineMatrixSVD(source, target)
+        transformed = transformAffine(source, matrix)
+        self.assertTrue(np.allclose(transformed, target, equal_nan=True))
         self.assertTrue(np.allclose(matrix, expected_mat, equal_nan=True))