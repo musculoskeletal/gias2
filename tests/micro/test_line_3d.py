264e153978c347623434148061c0552f520a6805
Ju Zhang <ju@formuslabs.com>
2021-03-01 23:07:48 +1300

    Fix Line3D intersection calculations
    - increase PRECISION tolerance to 1e-14. This is the smallest order of
      magnitude where rounding errors do not effect collinear and
      intersection calculation
    - Calculate line intersection using closest distance between points,
      intersections in all 3 dimensions are correctly detected
    - collinearity check, raises CollinearError now if lines are collinear
      during intersection check

--- tests/micro/test_line_3d.py
+++ tests/micro/test_line_3d.py
@@ -4,9 +4,9 @@ from generator import generator, generate
 from numpy import array as array
 from numpy.linalg import norm
 
-from gias2.common.geoprimitives import Line3D, NonInterceptError
+from gias2.common.geoprimitives import Line3D, NonInterceptError, CollinearError, PRECISION
 
-tol = 1e-16
+tol = PRECISION
 
 
 @generator
@@ -19,28 +19,30 @@ class TestLine3D(unittest.TestCase):
     """
 
     # The below cases should have intersects (hand calculated)
-    # A hypothesis as to why these functions may not work is that the current version of Gias2's
-    # geoprimitives modules only uses the x and y data of each direction vector. This leaves out data
-    # from the z axis. Whether or not this has an effect on the accuracy of computation is yet to be tested.
     @generate(
         ('Standard case1', [1, 0, 0], [0, 0, 0], [1, 0, -1], [0, 0, 1], [1, 0, 0]),
         ('Case1 in Z axis', [0, 0, 1], [0, 0, 0], [1, 0, -1], [0, 0, 1], [0, 0, 1]),
         ('Standard case2', [1, 0, 1], [0, 0, 0], [1, 0, 0], [0, 0, 1], [1, 0, 1]),
-        ('Case2, off-origin', [1, 0, 1], [-1, -1, -1], [1, 0, 0], [-1, -1, 0], [1, 0, 1]),
+        ('Case2, off-origin', [1, 0, 1], [-1, -1, -1], [1, 0, 0], [-1, -1, 0], [0, -1, 0]),
         ('Standard case3', [1, 1, 0], [0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]),
         ('Standard case4', [-1, -1, 0], [0, 0, 0], [-1, 0, 0], [0, -1, 0], [-1, -1, 0]),
         ('Case 4, off-origin', [-1, -1, 0], [1, 1, 1], [-1, 0, 0], [1, 0, 1], [0, 0, 1]),
         ('Standard case5', [1, 1, 1], [0, 0, 0], [1, 1, 0], [0, 0, 7], [7, 7, 7]),
-        ('Case5, off-origin', [1, 1, 1], [1, 1, 1], [1, 1, 0], [1, 1, 8], [7, 7, 7])
+        ('Case5, off-origin', [1, 1, 1], [1, 1, 1], [1, 1, 0], [1, 1, 8], [8, 8, 8])
     )
-    def test_calcIntercept_intercept_exists(self, name: str, ray1: list, origin1: list, ray2: list, origin2: list,
-                                            expected: list):
+    def test_calcIntercept_intercept_exists(
+            self,
+            name: str,
+            ray1: list,
+            origin1: list,
+            ray2: list,
+            origin2: list,
+            expected: list):
         line_a = Line3D(array(ray1), array(origin1))
         line_b = Line3D(array(ray2), array(origin2))
         self.assertTrue(norm(line_a.calcIntercept(line_b)[0] - array(expected)) < tol)
 
-    # Gias2 currently does not distinguish between non-interept and collinear cases
-    # TODO treat collinear lines differently to non-intersecting lines
+    # These lines are collinear and should result in the CollinearError exception being raised
     @generate(
         ('Same line (collinear) 1D (x)', [1, 0, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0]),
         ('Same line (collinear) 1D (y)', [0, 1, 0], [0, 0, 0], [0, 1, 0], [0, 0, 0]),
@@ -49,14 +51,19 @@ class TestLine3D(unittest.TestCase):
         ('Same direction, different origin (collinear) 1D (y)', [0, 1, 0], [0, 0, 0], [0, 1, 0], [0, 1, 0]),
         ('Same direction, different origin (collinear) 1D (z)', [0, 0, 1], [0, 0, 0], [0, 0, 1], [0, 0, 1]),
         ('Same direction, different origin (collinear) 2D (xy)', [1, 1, 0], [0, 0, 0], [-1, -1, 0], [1, 1, 0]),
-        ('Same direction, different origin (collinear) 2D (xz)', [1, 0, 1], [0, 0, 0], [-1, 0, -1], [1, 0, -1]),
+        ('Same direction, different origin (collinear) 2D (xz)', [1, 0, 1], [0, 0, 0], [-1, 0, -1], [2, 0, 2]),
         ('Same direction, different origin (collinear) 2D (yz)', [0, 1, 1], [0, 0, 0], [0, -1, -1], [0, 1, 1])
     )
-    def test_calcIntercept_errors_trivial_cases_collinear(self, name: str, ray1: list, origin1: list, ray2: list,
-                                                          origin2: list):
+    def test_calcIntercept_errors_trivial_cases_collinear(
+            self,
+            name: str,
+            ray1: list,
+            origin1: list,
+            ray2: list,
+            origin2: list):
         line_a = Line3D(array(ray1), array(origin1))
         line_b = Line3D(array(ray2), array(origin2))
-        self.assertRaises(NonInterceptError, lambda: line_a.calcIntercept(line_b))
+        self.assertRaises(CollinearError, lambda: line_a.calcIntercept(line_b))
 
     # These tests do not have intersects, so should pass when given a NonInterceptError.
     @generate(
@@ -64,7 +71,13 @@ class TestLine3D(unittest.TestCase):
         ('Anti-Parallel lines, different origin', [1, 0, 0], [0, 0, 0], [-1, 0, 0], [1, 1, 1]),
         ('Orthogonal rays, different origins', [1, 0, 0], [0, 0, 0], [0, 1, 0], [1, 1, 1])
     )
-    def test_calcIntercept_no_intercept(self, name: str, ray1: list, origin1: list, ray2: list, origin2: list):
+    def test_calcIntercept_no_intercept(
+            self,
+            name: str,
+            ray1: list,
+            origin1: list,
+            ray2: list,
+            origin2: list):
         line_a = Line3D(array(ray1), array(origin1))
         line_b = Line3D(array(ray2), array(origin2))
         self.assertRaises(NonInterceptError, lambda: line_a.calcIntercept(line_b))
